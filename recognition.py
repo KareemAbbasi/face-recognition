581ce7023855b0e6a351bc0a8be2a993d8086d8b
Kareem Abbasi <kareem.abbasi@mail.huji.ac.il>
2020-03-21 16:40:32 +0200

    added webcam recognition and overlaying the face with another image

--- recognition.py
+++ recognition.py
@@ -1,29 +1,53 @@
 import sys
 import cv2
 
-
-imagePath = sys.argv[1]
-cascPath = sys.argv[2]
-
+#Loading the haar cascade
+cascPath = sys.argv[1]
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#starting the webcam
+video_capture = cv2.VideoCapture(0)
+
+#the image to put on top of the face
+s_img = cv2.imread("poop.png", -1)
+
+while True:
+    ret, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #face detection
+    faces = faceCascade.detectMultiScale(
+        gray, 
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    for (x, y, w, h) in faces:
+        #draw rectangle around detected face
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+        
+        #resizing the overlaying image to the size of the face
+        s_img = cv2.resize(s_img, dsize=(w, h), interpolation=cv2.INTER_CUBIC)
+        
+        #putting the new image on top of the face without its background
+        y1, y2 = y, y + s_img.shape[0]
+        x1, x2 = x, x + s_img.shape[1]
+
+        alpha_s = s_img[:, :, 3] / 255.0
+        alpha_l = 1.0 - alpha_s
+
+        for c in range(0, 3):
+            frame[y1:y2, x1:x2, c] = (alpha_s * s_img[:, :, c] +
+                                    alpha_l * frame[y1:y2, x1:x2, c])
 
 
-faces = faceCascade.detectMultiScale(
-    gray, 
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30),
-    flags=cv2.CASCADE_SCALE_IMAGE
-)
+    cv2.imshow('Video', frame)
 
-print("Found {0} faces!".format(len(faces)))
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x,y), (x+w, y+h), (0, 255, 0), 2)
-
-cv2.imshow("Faces found", image)
-cv2.waitKey(0)    
+video_capture.release()
+cv2.destroyAllWindows() 
 
